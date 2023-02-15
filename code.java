c639615e69b8bab01965618940f46cc74e984647
kamesh 20 <kamesh20k@gmail.com>
2023-02-15 08:02:20 +0000

    3rd commit

--- code.java
+++ code.java
@@ -3,7 +3,7 @@ public class PrintAsciiValueExample2
 public static void main(String[] String)   
 {  
 int ch1 = 'a';  
-int ch2 = 'c';  
+int ch2 = 'a';  
 System.out.println("The ASCII value of a is: "+ch1);  
 System.out.println("The ASCII value of b is: "+ch2);  
 }  
